227d6371a0adb0c8ae8afe5d73643d379da6afad
Kenny Chu <kennychu@asl.com.hk>
2023-08-17 17:43:13 +0800

    refactor get styles function

--- src/helper.ts
+++ src/helper.ts
@@ -1,5 +1,6 @@
+//@ts-nocheck
+
 import { parse } from '@babel/parser';
-import { CallExpression, Expression, File, Identifier, MemberExpression, ObjectExpression, ObjectProperty, PrivateName } from '@babel/types';
 
 export function getStyles(text: string) {
   const ast = parse(text, {
@@ -15,43 +16,35 @@ export function getStyles(text: string) {
     if (node.type === 'VariableDeclaration') {
       node.declarations.forEach((item) => {
         if (item.type === 'VariableDeclarator') {
-          const init = item.init as CallExpression;
-          const callee = init?.callee as MemberExpression;
-          const obj = callee?.object as Identifier;
-          const property = callee?.property as Identifier;
+          const init = item.init;
+          const callee = init?.callee;
+          const obj = callee?.object;
+          const property = callee?.property;
+  
+          const id = item.id;
+          styleObjectLoc = id.loc;
+          globalStyleName = id.name;
+          let properties = null;
+  
           if (init.type === 'CallExpression') {
             if (obj?.name === 'StyleSheet' && property?.name === 'create') {
-              const id = item.id as Identifier;
-              globalStyleName = id.name;
-              //@ts-ignore
-              init?.arguments[0].properties.forEach((item) => {
-                const name = item.key.name;
-                styles[name] = {
-                  usage: 0,
-                  details: {
-                    item,
-                  },
-                };
-              });
+              properties = init?.arguments[0].properties;
             }
           } else if (init.type === 'ArrowFunctionExpression') {
-            //@ts-ignore
             if (init.body.callee.object.name === 'StyleSheet') {
-              //@ts-ignore
-              init.body.arguments[0].properties.forEach((item) => {
-                const name = item.key.name;
-                styles[name] = {
-                  usage: 0,
-                  details: {
-                    item,
-                  },
-                };
-              });
-              //@ts-ignore
-              globalStyleName = item.id.name ?? '';
-              stylesType = 'arrow';
+              properties = init.body.arguments[0].properties;
             }
+            stylesType = 'arrow';
           }
+          properties?.forEach((item) => {
+            const name = item.key.name;
+            styles[name] = {
+              usage: 0,
+              details: {
+                item,
+              },
+            };
+          });
         }
       });
     }
