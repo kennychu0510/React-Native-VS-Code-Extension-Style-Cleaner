84d43793ad61276b29c071ee2d697ee47711163b
Kenny Chu <kennychu@asl.com.hk>
2024-02-05 10:23:16 +0800

    added test scenario for selecting invalid style

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -5,6 +5,7 @@ import * as fs from 'fs'
 // as well as import your extension to test it
 import * as vscode from 'vscode';
 // import * as myExtension from '../extension';
+import * as sinon from 'sinon';
 
 suite('Extract style into stylesheet', () => {
   test('Scenario 1: Stylesheet contains at least one item', async () => {
@@ -65,6 +66,41 @@ suite('Extract style into stylesheet', () => {
     await vscode.commands.executeCommand('RNStylesCleaner.extractSelectionIntoStyleSheet', 'container');
 
     await vscode.commands.executeCommand('workbench.action.files.save');
+
+    // get content of current file
+    const currentFile = fs.readFileSync(filePath, 'utf8');
+
+    // assert beforeFile not equal after file
+    assert.strictEqual(currentFile, afterFile);
+  });
+
+  test('Scenario 3: Selection is invalid style', async () => {
+    const showErrorMessageSpy = sinon.spy(vscode.window, 'showErrorMessage');
+
+    // Get the current workspace directory
+    const workspaceFolder = vscode.workspace.workspaceFolders![0].uri.fsPath;
+    const filePath = path.join(workspaceFolder, 'invalidStyle.js');
+    const beforeFile = fs.readFileSync(path.join(workspaceFolder, 'invalidStyle-before.js'), 'utf8');
+    const afterFile = fs.readFileSync(path.join(workspaceFolder, 'invalidStyle-after.js'), 'utf8');
+
+    // create new file for testing
+    fs.writeFileSync(filePath, beforeFile, 'utf8');
+    
+    // Open the file in the file explorer
+    const uri = vscode.Uri.file(filePath);
+    const document = await vscode.workspace.openTextDocument(uri);
+    await vscode.commands.executeCommand('RNStylesCleaner-sidebar.focus')
+    await vscode.window.showTextDocument(document);
+    
+
+    // select line 6 col 5 to line 6 col 29
+    const selection = new vscode.Selection(5, 4, 5, 28);
+    vscode.window.activeTextEditor!.selection = selection;
+    await sleep()
+
+    await vscode.commands.executeCommand('RNStylesCleaner.extractSelectionIntoStyleSheet');
+
+    assert.strictEqual(showErrorMessageSpy.calledOnce, true);
 
     // get content of current file
     const currentFile = fs.readFileSync(filePath, 'utf8');
