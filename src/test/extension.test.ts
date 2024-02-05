b0040a567e14c828fa2b9219d68719f518468618
Kenny Chu <kennychu0510@gmail.com>
2024-02-05 20:28:07 +0800

    reorganise test resources

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -5,6 +5,12 @@ import * as vscode from 'vscode';
 import * as sinon from 'sinon';
 import * as mocha from 'mocha';
 
+const features = {
+  styleExtraction: 'styles-extraction',
+  copyStyles: 'copy-styles',
+  removeStyles: 'remove-styles',
+} as const;
+
 suite('RN Styles Cleaner', () => {
   const showErrorMessageSpy = sinon.spy(vscode.window, 'showErrorMessage');
 
@@ -13,9 +19,9 @@ suite('RN Styles Cleaner', () => {
   test('Extract styles - Scenario 1: Stylesheet contains at least one item', async () => {
     const scenario = 'one-style';
     const workspaceFolder = vscode.workspace.workspaceFolders![0].uri.fsPath;
-    const filePath = path.join(workspaceFolder, scenario, 'working.js');
-    const beforeFile = fs.readFileSync(path.join(workspaceFolder, scenario, 'before.js'), 'utf8');
-    const afterFile = fs.readFileSync(path.join(workspaceFolder, scenario, 'after.js'), 'utf8');
+    const filePath = path.join(workspaceFolder, features.styleExtraction, scenario, 'working.js');
+    const beforeFile = fs.readFileSync(path.join(workspaceFolder, features.styleExtraction, scenario, 'before.js'), 'utf8');
+    const afterFile = fs.readFileSync(path.join(workspaceFolder, features.styleExtraction, scenario, 'after.js'), 'utf8');
 
     // create new file for testing
     fs.writeFileSync(filePath, beforeFile, 'utf8');
@@ -45,9 +51,9 @@ suite('RN Styles Cleaner', () => {
   test('Extract styles - Scenario 2: No stylesheet', async () => {
     const scenario = 'no-style';
     const workspaceFolder = vscode.workspace.workspaceFolders![0].uri.fsPath;
-    const filePath = path.join(workspaceFolder, scenario, 'working.js');
-    const beforeFile = fs.readFileSync(path.join(workspaceFolder, scenario, 'before.js'), 'utf8');
-    const afterFile = fs.readFileSync(path.join(workspaceFolder, scenario, 'after.js'), 'utf8');
+    const filePath = path.join(workspaceFolder, features.styleExtraction, scenario, 'working.js');
+    const beforeFile = fs.readFileSync(path.join(workspaceFolder, features.styleExtraction, scenario, 'before.js'), 'utf8');
+    const afterFile = fs.readFileSync(path.join(workspaceFolder, features.styleExtraction, scenario, 'after.js'), 'utf8');
 
     // create new file for testing
     fs.writeFileSync(filePath, beforeFile, 'utf8');
@@ -78,9 +84,9 @@ suite('RN Styles Cleaner', () => {
     const scenario = 'multi-line-selection';
 
     const workspaceFolder = vscode.workspace.workspaceFolders![0].uri.fsPath;
-    const filePath = path.join(workspaceFolder, scenario, 'working.js');
-    const beforeFile = fs.readFileSync(path.join(workspaceFolder, scenario, 'before.js'), 'utf8');
-    const afterFile = fs.readFileSync(path.join(workspaceFolder, scenario, 'after.js'), 'utf8');
+    const filePath = path.join(workspaceFolder, features.styleExtraction, scenario, 'working.js');
+    const beforeFile = fs.readFileSync(path.join(workspaceFolder, features.styleExtraction, scenario, 'before.js'), 'utf8');
+    const afterFile = fs.readFileSync(path.join(workspaceFolder, features.styleExtraction, scenario, 'after.js'), 'utf8');
 
     // create new file for testing
     fs.writeFileSync(filePath, beforeFile, 'utf8');
@@ -113,9 +119,9 @@ suite('RN Styles Cleaner', () => {
     const scenario = 'invalid-style';
 
     const workspaceFolder = vscode.workspace.workspaceFolders![0].uri.fsPath;
-    const filePath = path.join(workspaceFolder, scenario, 'working.js');
-    const beforeFile = fs.readFileSync(path.join(workspaceFolder, scenario, 'before.js'), 'utf8');
-    const afterFile = fs.readFileSync(path.join(workspaceFolder, scenario, 'after.js'), 'utf8');
+    const filePath = path.join(workspaceFolder, features.styleExtraction, scenario, 'working.js');
+    const beforeFile = fs.readFileSync(path.join(workspaceFolder, features.styleExtraction, scenario, 'before.js'), 'utf8');
+    const afterFile = fs.readFileSync(path.join(workspaceFolder, features.styleExtraction, scenario, 'after.js'), 'utf8');
 
     // create new file for testing
     fs.writeFileSync(filePath, beforeFile, 'utf8');
@@ -146,9 +152,9 @@ suite('RN Styles Cleaner', () => {
     const scenario = 'extract-multiple-root-styles';
 
     const workspaceFolder = vscode.workspace.workspaceFolders![0].uri.fsPath;
-    const filePath = path.join(workspaceFolder, scenario, 'working.js');
-    const beforeFile = fs.readFileSync(path.join(workspaceFolder, scenario, 'before.js'), 'utf8');
-    const afterFile = fs.readFileSync(path.join(workspaceFolder, scenario, 'after.js'), 'utf8');
+    const filePath = path.join(workspaceFolder, features.styleExtraction, scenario, 'working.js');
+    const beforeFile = fs.readFileSync(path.join(workspaceFolder, features.styleExtraction, scenario, 'before.js'), 'utf8');
+    const afterFile = fs.readFileSync(path.join(workspaceFolder, features.styleExtraction, scenario, 'after.js'), 'utf8');
 
     // create new file for testing
     fs.writeFileSync(filePath, beforeFile, 'utf8');
@@ -182,9 +188,9 @@ suite('RN Styles Cleaner', () => {
     const scenario = 'clean-style-1';
     const workspaceFolder = vscode.workspace.workspaceFolders![0].uri.fsPath;
 
-    const filePath = path.join(workspaceFolder, scenario, 'working.js');
-    const beforeFile = fs.readFileSync(path.join(workspaceFolder, scenario, 'before.js'), 'utf8');
-    const afterFile = fs.readFileSync(path.join(workspaceFolder, scenario, 'after.js'), 'utf8');
+    const filePath = path.join(workspaceFolder, features.removeStyles, scenario, 'working.js');
+    const beforeFile = fs.readFileSync(path.join(workspaceFolder, features.removeStyles, scenario, 'before.js'), 'utf8');
+    const afterFile = fs.readFileSync(path.join(workspaceFolder, features.removeStyles, scenario, 'after.js'), 'utf8');
 
     // create new file for testing
     fs.writeFileSync(filePath, beforeFile, 'utf8');
@@ -209,9 +215,9 @@ suite('RN Styles Cleaner', () => {
     const scenario = 'clean-style-no-unused';
     const workspaceFolder = vscode.workspace.workspaceFolders![0].uri.fsPath;
 
-    const filePath = path.join(workspaceFolder, scenario, 'working.js');
-    const beforeFile = fs.readFileSync(path.join(workspaceFolder, scenario, 'before.js'), 'utf8');
-    const afterFile = fs.readFileSync(path.join(workspaceFolder, scenario, 'after.js'), 'utf8');
+    const filePath = path.join(workspaceFolder, features.removeStyles, scenario, 'working.js');
+    const beforeFile = fs.readFileSync(path.join(workspaceFolder, features.removeStyles, scenario, 'before.js'), 'utf8');
+    const afterFile = fs.readFileSync(path.join(workspaceFolder, features.removeStyles, scenario, 'after.js'), 'utf8');
 
     // create new file for testing
     fs.writeFileSync(filePath, beforeFile, 'utf8');
@@ -236,9 +242,9 @@ suite('RN Styles Cleaner', () => {
     const scenario = 'clean-style-no-styles';
     const workspaceFolder = vscode.workspace.workspaceFolders![0].uri.fsPath;
 
-    const filePath = path.join(workspaceFolder, scenario, 'working.js');
-    const beforeFile = fs.readFileSync(path.join(workspaceFolder, scenario, 'before.js'), 'utf8');
-    const afterFile = fs.readFileSync(path.join(workspaceFolder, scenario, 'after.js'), 'utf8');
+    const filePath = path.join(workspaceFolder, features.removeStyles, scenario, 'working.js');
+    const beforeFile = fs.readFileSync(path.join(workspaceFolder, features.removeStyles, scenario, 'before.js'), 'utf8');
+    const afterFile = fs.readFileSync(path.join(workspaceFolder, features.removeStyles, scenario, 'after.js'), 'utf8');
 
     // create new file for testing
     fs.writeFileSync(filePath, beforeFile, 'utf8');
@@ -265,9 +271,9 @@ suite('RN Styles Cleaner', () => {
     const scenario = 'clean-style-2';
     const workspaceFolder = vscode.workspace.workspaceFolders![0].uri.fsPath;
 
-    const filePath = path.join(workspaceFolder, scenario, 'working.js');
-    const beforeFile = fs.readFileSync(path.join(workspaceFolder, scenario, 'before.js'), 'utf8');
-    const afterFile = fs.readFileSync(path.join(workspaceFolder, scenario, 'after.js'), 'utf8');
+    const filePath = path.join(workspaceFolder, features.removeStyles, scenario, 'working.js');
+    const beforeFile = fs.readFileSync(path.join(workspaceFolder, features.removeStyles, scenario, 'before.js'), 'utf8');
+    const afterFile = fs.readFileSync(path.join(workspaceFolder, features.removeStyles, scenario, 'after.js'), 'utf8');
 
     // create new file for testing
     fs.writeFileSync(filePath, beforeFile, 'utf8');
@@ -293,9 +299,9 @@ suite('RN Styles Cleaner', () => {
     const scenario = 'clean-style-multiple';
     const workspaceFolder = vscode.workspace.workspaceFolders![0].uri.fsPath;
 
-    const filePath = path.join(workspaceFolder, scenario, 'working.js');
-    const beforeFile = fs.readFileSync(path.join(workspaceFolder, scenario, 'before.js'), 'utf8');
-    const afterFile = fs.readFileSync(path.join(workspaceFolder, scenario, 'after.js'), 'utf8');
+    const filePath = path.join(workspaceFolder, features.removeStyles, scenario, 'working.js');
+    const beforeFile = fs.readFileSync(path.join(workspaceFolder, features.removeStyles, scenario, 'before.js'), 'utf8');
+    const afterFile = fs.readFileSync(path.join(workspaceFolder, features.removeStyles, scenario, 'after.js'), 'utf8');
 
     // create new file for testing
     fs.writeFileSync(filePath, beforeFile, 'utf8');
@@ -324,9 +330,9 @@ suite('RN Styles Cleaner', () => {
     const scenario = 'copy-styles-no-selection';
     const workspaceFolder = vscode.workspace.workspaceFolders![0].uri.fsPath;
 
-    const filePath = path.join(workspaceFolder, scenario, 'working.js');
-    const beforeFile = fs.readFileSync(path.join(workspaceFolder, scenario, 'before.js'), 'utf8');
-    const afterFile = fs.readFileSync(path.join(workspaceFolder, scenario, 'after.js'), 'utf8');
+    const filePath = path.join(workspaceFolder, features.copyStyles, scenario, 'working.js');
+    const beforeFile = fs.readFileSync(path.join(workspaceFolder, features.copyStyles, scenario, 'before.js'), 'utf8');
+    const afterFile = fs.readFileSync(path.join(workspaceFolder, features.copyStyles, scenario, 'after.js'), 'utf8');
 
     // create new file for testing
     fs.writeFileSync(filePath, beforeFile, 'utf8');
@@ -360,9 +366,9 @@ suite('RN Styles Cleaner', () => {
     const scenario = 'copy-styles-one-line';
     const workspaceFolder = vscode.workspace.workspaceFolders![0].uri.fsPath;
 
-    const filePath = path.join(workspaceFolder, scenario, 'working.js');
-    const beforeFile = fs.readFileSync(path.join(workspaceFolder, scenario, 'before.js'), 'utf8');
-    const afterFile = fs.readFileSync(path.join(workspaceFolder, scenario, 'after.js'), 'utf8');
+    const filePath = path.join(workspaceFolder, features.copyStyles, scenario, 'working.js');
+    const beforeFile = fs.readFileSync(path.join(workspaceFolder, features.copyStyles, scenario, 'before.js'), 'utf8');
+    const afterFile = fs.readFileSync(path.join(workspaceFolder, features.copyStyles, scenario, 'after.js'), 'utf8');
 
     // create new file for testing
     fs.writeFileSync(filePath, beforeFile, 'utf8');
